786a489830ba87c6b3de46fe0a9d56d82ce4f3a9
Ashley Brown <fay.ashbro@gmail.com>
2024-03-07 11:40:34 -0500

    removing keys

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-import { Configuration, OpenAIApi } from "openai";
-import readline from "readline";
 
-
-
-const configuration = new Configuration({
-    organization: "org-mv3F72my6wcvKgr032IgNM8X",
-    apiKey: "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",
-  });
-  
-
-const openai = new OpenAIApi(configuration);
